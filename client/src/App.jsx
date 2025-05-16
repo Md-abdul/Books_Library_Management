e8a45375e1036efafe466d4e9e02ff65e0c0d457
Md-abdul <mdabdulq62@gmail.com>
2025-05-17 00:15:21 +0530

    start

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 class="text-3xl font-bold underline">Hello</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
